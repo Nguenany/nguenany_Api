8c1d73404b7b88d90909a566a44fa1a90b4cd385
nguenany <nguenany@live.com>
2024-02-28 13:15:17 +0100

    Projecto Spring Boot

---
+++ src/test/java/nguenany/gmail/com/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nguenany.gmail.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
